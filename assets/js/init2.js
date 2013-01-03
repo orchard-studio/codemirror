548e6496235b79991be42f048d459dc58df248b4
Andrew Minton <me@andrewminton.co.uk>
2013-01-03 21:03:36 +0000

    Oops extra code in init2.js removed

--- assets/js/init2.js
+++ assets/js/init2.js
@@ -32,47 +32,4 @@ jQuery(document).ready(function () {
 	}
 	
 });
-
-
-
-      CodeMirror.xmlHints['<'] = [
-          'levelTop',
-          'levelRoot',
-          'mainLevel'
-      ];
-
-      CodeMirror.xmlHints['<levelTop '] = 
-      CodeMirror.xmlHints['<levelRoot '] = 
-      CodeMirror.xmlHints['<mainLevel '] = [
-          'property1111',
-          'property2222'
-      ];
-
-      CodeMirror.xmlHints['<levelTop><'] = 
-      CodeMirror.xmlHints['<levelRoot><'] = 
-      CodeMirror.xmlHints['<mainLevel><'] = [
-          'second',
-          'two'
-      ];
-
-      CodeMirror.xmlHints['<levelTop><second '] = [
-        'secondProperty'
-      ];
-
-      CodeMirror.xmlHints['<levelTop><second><'] = [
-        'three',
-        'x-three'
-      ];
-
-      var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-            value: '',
-            mode: 'text/html',
-            lineNumbers: true,
-            extraKeys: {
-                "' '": function(cm) { CodeMirror.xmlHint(cm, ' '); },
-                "'<'": function(cm) { CodeMirror.xmlHint(cm, '<'); },
-                "Ctrl-Space": function(cm) { CodeMirror.xmlHint(cm, ''); }
-            },
-            autoCloseTags: true
-        });
     
