9917774c97a83c4c8e70f9bd2dc0fb920e87012a
Andrew Minton <me@andrewminton.co.uk>
2013-01-04 13:26:34 +0000

    Tidying up declaration of Emmet code

--- assets/js/init.js
+++ assets/js/init.js
@@ -12,19 +12,19 @@ jQuery(document).ready(function () {
       });*/
 	  
 	  
-      var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-		value: '',
-		  mode: "text/html",
-		  tabMode: "indent",
-		  lineNumbers: true,
-		  autoClearEmptyLines: true,
-		  autofocus: true,
-		  profile: 'xhtml',
-          autoCloseTags: true
-		  
-		  });
-		 
-		  var hlLine = editor.addLineClass(0, "background", "activeline");
+		var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+			value: '',
+			mode: "text/html",
+			//tabMode: "indent",
+			lineNumbers: true,
+			autoClearEmptyLines: true,
+			autofocus: true,
+			profile: 'xml',
+			autoCloseTags: true
+			  
+		});
+			 
+		var hlLine = editor.addLineClass(0, "background", "activeline");
 			editor.on("cursorActivity", function() {
 				editor.matchHighlight("CodeMirror-matchhighlight");
 				var cur = editor.getLineHandle(editor.getCursor().line);
